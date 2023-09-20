689f0b6fcddce96948d56e8f649b0feebf5e9474
MkGee <matthewrmcgeehan2@gmail.com>
2023-09-20 14:55:23 -0400

    Add files via upload

---
+++ list_building.py
@@ -0,0 +1,11 @@
+list = ['larry', 'curly', 'moe']
+list.append('shemp')
+list.insert(0, 'xxx')
+list.extend(['yyy', 'xxx'])
+print(list)
+print(list.index('curly'))
+
+list.remove('curly')
+list.pop(1)
+print(list)
+
---
+++ sorting.py
@@ -0,0 +1,11 @@
+#sorting
+
+a = [5, 1, 4, 3]
+print(sorted(a)) # [1, 3, 4, 5]
+print(a) #[5, 1, 4, 3]
+
+
+strs = ['aa', 'BB', 'zz', 'CC']
+print(sorted(strs)) #['BB', 'CC', 'aa', 'zz'] case sensitive
+print(sorted(strs, reverse=True)) #['zz', 'aa', 'CC', 'BB']
+
